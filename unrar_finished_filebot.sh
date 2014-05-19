1c877ea57b985c4f8483fef56e958afa4e307b4e
Gutz-Pilz <hannes.mueller87@gmail.com>
2014-05-19 15:34:48 +0200

    Update unrar_finished_filebot.sh

--- unrar_finished_filebot.sh
+++ unrar_finished_filebot.sh
@@ -6,9 +6,8 @@ BaseDir=/media/5a24e136-09b9-48e1-95db-b44d5db3e28a
 DownloadDir=${BaseDir}/Medien/Downloads
 MediaDir=${BaseDir}/Medien
 LogFile=/root/.pyload/Logs/log.txt 			# LogFile
-LockFile=/root/.pyload/Logs/filebot.lock                # LockFile
+LockFile=/root/.pyload/Logs/filebot.lock		# LockFile
 ExtScript=filebot.sh
-
 
 #Pyload
 DownloadFolder=$MediaDir/$1
@@ -18,7 +17,7 @@ SERVICE2=filebot
 #FileBot-defs
 MovieFormat="movieFormat=Movies/{net.sourceforge.filebot.WebServices.TMDb.getMovieInfo(movie, Locale.GERMAN).name} {'('+y+')'}/{net.sourceforge.filebot.WebServices.TMDb.getMovieInfo(movie, Locale.GERMAN).name} {'('+y+')'}"
 Ignore="ignore=\b(?i:doku)\b"
-Execute="exec=cd /root/ && ./$ExtScript \"{file}\""
+Execute="exec=cd / && ./$ExtScript \"{file}\""
 Extras="clean=y artwork=n"
 
 
@@ -27,14 +26,16 @@ logit(){
 	echo -e "$logline " $* | tee -a $LogFile
 	return 0
 }
+
 if grep $1 $LockFile; then
     logit "ABORT! Already processed that Directory! ($1)"
     exit
 else
-
         logit "##########################"
 	logit "########### unrar_finished"
-	count=`find "$DownloadFolder" -name "*.rar" -o -name "*.r0*" 2>/dev/null | wc -l`
+	#count=`find "$DownloadFolder" -name "*.rar" -o -name "*.r0*" 2>/dev/null | wc -l`
+        cd "$DownloadFolder"
+	count=`ls -1 *.rar 2>/dev/null | wc -l`
 	if [ $count != 0 ]
 	then
 		logit "ABORT! Still some Archives"
@@ -51,10 +52,10 @@ else
 		filebot -script fn:cleaner "$DownloadFolder" --def root=y "$Ignore" "exts=jpg|nfo|rar|etc" "terms=sample|trailer|etc"
 		}
 		xbmc_clean(){
-		curl -s -d '{"jsonrpc":"2.0","method":"VideoLibrary.Clean","id":1}' -H 'content-type: application/json;' http://192.168.0.107:8585/jsonrpc?VideoLibrary.Clean
+		curl -s -d '{"jsonrpc":"2.0","method":"VideoLibrary.Clean","id":1}' -H 'content-type: application/json;' http://192.168.0.107:8080/jsonrpc?VideoLibrary.Clean
 		}
 		xbmc_scan(){
-		curl -s -d '{"jsonrpc":"2.0","method":"VideoLibrary.Scan","id":1}' -H 'content-type: application/json;' http://192.168.0.107:8585/jsonrpc?VideoLibrary.Scan
+		curl -s -d '{"jsonrpc":"2.0","method":"VideoLibrary.Scan","id":1}' -H 'content-type: application/json;' http://192.168.0.107:8080/jsonrpc?VideoLibrary.Scan
 		}
 
 		# Ausfuehren #
@@ -63,12 +64,12 @@ else
 		sortiere
 		logit "cleaning Clutter with FileBot"
 		cleaning
-		logit "reset Lock-File"
-                echo "" > $LockFile
-		#logit "XBMC clean"
-		#xbmc_clean
-		#logit "XBMC scan"
-		#xbmc_scan
+		logit "XBMC clean"
+		xbmc_clean
+		logit "XBMC scan"
+		xbmc_scan
+		logit "reset Lock-File in 10 min"
+		(sleep 600; echo "" > $LockFile) &
                 exit
         fi
 fi
