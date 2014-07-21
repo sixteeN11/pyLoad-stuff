e40987999f151830498a2c48a390a17f94c7bbb3
Gutz-Pilz <hannes.mueller87@gmail.com>
2014-07-21 15:45:09 +0200

    Create filebot.sh

---
+++ filebot.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Globale Variablen #
+BaseDir=/media/5a24e136-09b9-48e1-95db-b44d5db3e28a
+DownloadDir=${BaseDir}/Medien/Downloads
+MediaDir=${BaseDir}/Medien
+
+## Log
+DATE=$(date +%d.%m.%Y\ %H:%M:%S)
+logline=$(date +'%d.%m.%Y')" "$(date +'%H:%M:%S')" FileBot"
+LogFile=/root/.pyload/Logs/log.txt
+## DTS2AC3 um den Datentransfer von OSDrive zu NASDrive zu umgehen (NASHD -> NASHD)
+##tmpFolder=$MediaDir/tmp
+
+## String Manipulation
+mailtitle_ext=${1##*/}
+mailtitle=${mailtitle_ext%.*}
+pfad_00=${1%/*.mkv}
+pfad=${pfad_00##*/Medien}
+
+        echo  "$logline ##########################" | tee -a $LogFile
+        echo  "$logline Dateihandling nachdem FILEBOT fertig ist" | tee -a $LogFile
+        echo  "$logline Datei wurde nach ~${1%/*.mkv}/* verschoben" | tee -a $LogFile
+        DUCMD="$(which \du) -m"
+        FileSize1=$($DUCMD "$1" | cut -f1)
+        cd /
+
+        echo "$logline Neues Erstelldatum" | tee -a $LogFile
+        touch -c "$1"
+
+        echo "$logline CHMOD 777" | tee -a $LogFile
+        chmod 777 "$1"
+        Final=$($DUCMD "$1" | cut -f1)
+        Final2=$(echo "$Final" | sed -e :a -e 's/\(.*[0-9]\)\([0-9]\{3\}\)/\1.\2/;ta')
+
+        echo "$logline XBMC Library Update" | tee -a $LogFile
+        docker run --net=host -v /root/xbmc-server/xbmcdata:/opt/xbmc-server/portable_data --entrypoint=/opt/xbmc-server/xbmcVideoLibraryScan wernerb/docker-xbmc-server --no-test --nolirc -p
+
+        echo "$logline E-Mail senden" | tee -a $LogFile
+        echo -e "Verschoben nach:\t ~/NAS_HD$pfad/\n\nGroesse:\t$Final2 MB\n\n\nSincerly\nyour lovely NAS" | mailx -s "INFO: $mailtitle runtergeladen" hannes.mueller87@gmail.com;
+
+        echo "$logline Lösche leere Ordner" | tee -a $LogFile
+        cd /root/.pyload/Downloads
+        find . -type d -empty -exec rmdir {} \;
+
