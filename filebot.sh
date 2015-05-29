7710945540ccecb442d4db25528e0467a8c3bdb6
Gutz-Pilz <hannes.mueller87@gmail.com>
2015-05-29 22:55:05 +0200

    changed because of FileBot has Log feature
    c

--- filebot.sh
+++ filebot.sh
@@ -1,47 +1,45 @@
 #!/bin/bash
-## Log
-DATE=$(date +%d.%m.%Y\ %H:%M:%S)
-logline=$(date +'%d.%m.%Y')" "$(date +'%H:%M:%S')" FileBot"
-LogFile=/root/.pyload/Logs/log.txt
 ## String Manipulation
 mailtitle_ext=${1##*/}
 mailtitle=${mailtitle_ext%.*}
 pfad_00=${1%/*.mkv}
 pfad=${pfad_00##*/Medien}
-API="nxxxxxx"
+
+##Pushbullet API
+API="n6n3Zb1wkwWhyTist1uX7GiAUdmLk8Ou"
 
 ## Schreib was in den pyLoad Log
-echo  "$logline ##########################" | tee -a $LogFile
-echo  "$logline Dateihandling nachdem FILEBOT fertig ist" | tee -a $LogFile
+echo  "ExternaScript:  ##########################"
+echo  "ExternaScript:  Dateihandling nachdem FILEBOT fertig ist"
 
 if [[ $1 == *".mkv" ]] || [[ $1 == *".avi" ]] || [[ $1 == *".mp4" ]]; then
-  echo  "$logline Datei wurde nach ~$pfad_00 verschoben" | tee -a $LogFile
-  ## Ermittle Dateigröße
+  echo  "ExternaScript:  Datei wurde nach ~$pfad_00 verschoben"
+  ## Ermittle Dateigroeße
   DUCMD="$(which \du) -m"
   FileSize1=$($DUCMD "$1" | cut -f1)
   cd /
 fi
 
-## Neues Erstelldatum, dass XBMC es unter "recently added" führt
-echo "$logline Neues Erstelldatum" | tee -a $LogFile
+## Neues Erstelldatum, dass XBMC es unter "recently added" fuehrt
+echo "ExternaScript:  Neues Erstelldatum"
 touch -c "$1"
 
 ## Vergebe der Datei alle Rechte
-echo "$logline CHMOD 777" | tee -a $LogFile
+echo "ExternaScript:  CHMOD 777"
 chmod 777 "$1"
 if [[ $1 == *".mkv" ]] || [[ $1 == *".avi" ]] || [[ $1 == *".mp4" ]]; then
   Final=$($DUCMD "$1" | cut -f1)
   Final2=$(echo "$Final" | sed -e :a -e 's/\(.*[0-9]\)\([0-9]\{3\}\)/\1.\2/;ta')
 fi
 
-## Lösche leere Ordner
-echo "$logline Lösche leere Ordner" | tee -a $LogFile
+## Loesche leere Ordner
+echo "ExternaScript:  Loesche leere Ordner"
 cd /root/.pyload/Downloads
 find . -type d -empty -exec rmdir {} \;
 
 ## Sende Push (aber nur Filmdateien)
 function push {
-  echo "$logline PUSH senden" | tee -a $LogFile
+  echo "ExternaScript:  PUSH senden"
   if [[ $2 == "online" ]]; then
     curl -u $API: https://api.pushbullet.com/v2/pushes -d type=note -d title="pyLoad: FileBot sorted '$mailtitle'" -d body="Server is online!%0Amoved File to: /NAS_Medien$pfad/%0A%0AFilesize: $Final2 MB" 2>&1 >/dev/null
   else
@@ -53,13 +51,13 @@ if [[ $1 == *".mkv" ]] || [[ $1 == *".avi" ]] || [[ $1 == *".mp4" ]]; then
 ## Sende Dateien an NAS (wenn er online ist)
   if ping -c4 192.168.0.107 2>&1 >/dev/null; then
     mount -a
-    echo "$logline NAS ist online - verschiebe Datei dorthin" | tee -a $LogFile
+    echo "ExternaScript:  NAS ist online - verschiebe Datei dorthin"
     push "$1" "online"
-    rsync --remove-source-files -rvh "$1" "/mnt/HD/NAS_Medien$pfad/" --exclude='Music' --exclude='TVHeadend' --exclude='Anime' | grep -E '*mkv|*avi|*mp4'
+    rsync --remove-source-files -rvh "$1" "/mnt/HD/NAS_Medien$pfad/" --exclude='Music' --exclude='TVHeadend' --exclude='Anime' | grep -E '*mkv|*avi|*mp4' 2>&1 >/dev/null
   else
-    echo "$logline NAS ist offline - verschiebe keine Daten" | tee -a $LogFile
+    echo "ExternaScript:  NAS ist offline - verschiebe keine Daten"
     push "$1" "offline"
   fi
 else
-  echo "$logline $mailtitle_ext ist kein Film"
+  echo "ExternaScript:  $mailtitle_ext ist kein Film"
 fi
