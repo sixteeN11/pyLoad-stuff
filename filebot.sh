c84f8637b7ddcc2479195a0ee5fbf98e7f58281c
Gutz-Pilz <hannes.mueller87@gmail.com>
2015-06-02 10:39:34 +0200

    Update filebot.sh

--- filebot.sh
+++ filebot.sh
@@ -6,17 +6,17 @@ pfad_00=${1%/*.mkv}
 pfad=${pfad_00##*/Medien}
 
 ##Pushbullet API
-API="nxxxxx"
+API="nxxxxxxxxxx"
 
 echo  "ExternalScript:  ##########################"
 echo  "ExternalScript:  Dateihandling nachdem FILEBOT fertig ist"
 if [ -f "$1" ];then
 	if [[ $1 == *".mkv" ]] || [[ $1 == *".avi" ]] || [[ $1 == *".mp4" ]]; then
-	  echo  "ExternalScript:  Datei wurde nach ~$pfad_00 verschoben"
-	  ## Ermittle Dateigroeße
-	  DUCMD="$(which \du) -m"
-	  FileSize1=$($DUCMD "$1" | cut -f1)
-	  cd /
+		echo  "ExternalScript:  Datei wurde nach ~$pfad_00 verschoben"
+		## Ermittle Dateigroeße
+		DUCMD="$(which \du) -m"
+		FileSize1=$($DUCMD "$1" | cut -f1)
+		cd /
 	fi
 
 	## Neues Erstelldatum, dass XBMC es unter "recently added" fuehrt
@@ -27,8 +27,8 @@ if [ -f "$1" ];then
 	echo "ExternalScript:  CHMOD 777"
 	chmod 777 "$1"
 	if [[ $1 == *".mkv" ]] || [[ $1 == *".avi" ]] || [[ $1 == *".mp4" ]]; then
-	  Final=$($DUCMD "$1" | cut -f1)
-	  Final2=$(echo "$Final" | sed -e :a -e 's/\(.*[0-9]\)\([0-9]\{3\}\)/\1.\2/;ta')
+		Final=$($DUCMD "$1" | cut -f1)
+		Final2=$(echo "$Final" | sed -e :a -e 's/\(.*[0-9]\)\([0-9]\{3\}\)/\1.\2/;ta')
 	fi
 
 	## Loesche leere Ordner
@@ -38,34 +38,33 @@ if [ -f "$1" ];then
 
 	## Sende Push (aber nur Filmdateien)
 	function push {
-	  echo "ExternalScript:  PUSH senden"
-	  if [[ $2 == "online" ]]; then
-		curl -u $API: https://api.pushbullet.com/v2/pushes -d type=note -d title="pyLoad: FileBot sorted '$mailtitle'" -d body="Server is online!%0Amoved File to: /NAS_Medien$pfad/%0A%0AFilesize: $Final2 MB" 2>&1 >/dev/null
-	  else
-		curl -u $API: https://api.pushbullet.com/v2/pushes -d type=note -d title="pyLoad: FileBot sorted '$mailtitle'" -d body="moved File to: /mnt/HD/Medien$pfad/%0A%0AFilesize: $Final2 MB" 2>&1 >/dev/null
-	  fi
+		echo "ExternalScript:  PUSH senden"
+		if [[ $2 == "online" ]]; then
+			curl -s  -u $API: https://api.pushbullet.com/v2/pushes -d type=note -d title="pyLoad: FileBot sorted '$mailtitle'" -d body="Server is online!%0Amoved File to: /NAS_Medien$pfad/%0A%0AFilesize: $Final2 MB" 2>&1 >/dev/null
+		else
+			curl -s  -u $API: https://api.pushbullet.com/v2/pushes -d type=note -d title="pyLoad: FileBot sorted '$mailtitle'" -d body="moved File to: /mnt/HD/Medien$pfad/%0A%0AFilesize: $Final2 MB" 2>&1 >/dev/null
+		fi
 	}
 
 	## Sende Dateien an NAS (wenn er online ist)
-	if ping -c4 192.168.0.107 2>&1 >/dev/null; then
+	if ping -c2 192.168.0.107 2>&1 >/dev/null; then
 		echo "ExternalScript:  NAS ist online - verschiebe Datei dorthin"
 		mount -a
-		rsync --remove-source-files -rvh "$1" "/mnt/HD/NAS_Medien$pfad/" --exclude='Music' --exclude='TVHeadend' --exclude='Anime' | grep -E '*mkv|*avi|*mp4' 2>&1 >/dev/null
+		rsync --remove-source-files -rvh "$1" "/mnt/HD/NAS_Medien$pfad/" --exclude='Music' --exclude='TVHeadend' --exclude='Anime'# | grep -E '*mkv|*avi|*mp4' 2>&1 >/dev/null
 		if [[ $1 == *".mkv" ]] || [[ $1 == *".avi" ]] || [[ $1 == *".mp4" ]]; then
 			push "$1" "online"
 		else
-			echo "ExternalScript:  $mailtitle_ext ist kein Film"
+			echo "ExternalScript:  Sende keine PushNachricht - '$mailtitle_ext' ist kein Film"
 		fi
 	else
 		echo "ExternalScript:  NAS ist offline - verschiebe keine Daten"
-		push "$1" "offline"
 		if [[ $1 == *".mkv" ]] || [[ $1 == *".avi" ]] || [[ $1 == *".mp4" ]]; then
 			push "$1" "offline"
 		else
-			echo "ExternalScript:  $mailtitle_ext ist kein Film"
+			echo "ExternalScript:  Sende keine PushNachricht - '$mailtitle_ext' ist kein Film"
 		fi
-	fi	
-
+	fi
 else
 	echo "ExternalScript:  No such File found '$1'"
 fi
+
