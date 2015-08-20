53b8d3cf9687db538d9ca920063e6dc28cc90862
Gutz-Pilz <hannes.mueller87@gmail.com>
2015-08-20 16:32:58 +0200

    Update Serienjunkies-RSS.py

--- Serienjunkies-RSS.py
+++ Serienjunkies-RSS.py
@@ -1,41 +1,16 @@
 #!/usr/bin/env python
 # -- coding: utf-8 --
-import feedparser, re, requests, codecs, os, sys
+import feedparser, re, requests, datetime
 from BeautifulSoup import BeautifulSoup
 
 ### Anpassen
-file = "SJ.txt"             # Datei für die gewünschten Serien
-rejectlist = "itunes;xxx"   # Was soll nicht im Titel vorkommen
-hoster = "ul"               # ul, filemonkey, cloudzer, share-online oder alle
+hoster = "ul"               # ul, so oder alle
 outputFilename = "rss.xml"  # wo soll die rss-datei gespeichert werden ?
 
-### Nur anpassen wenn Regex auf False
-regex = True                # Eintraege aus der Suchdatei als regulaere Ausdruecke behandeln
-debug = False               # Sehen was abgelehnt wird
-language = "DEUTSCH"        # welche sprache "DEUTSCH" oder "ENGLISCH" - spielt nur eine Rolle wenn regex auf False
-quality= "720p"             # 480p;720p;1080p - spielt nur eine Rolle wenn regex auf False
+def formatDate(dt):
+    return dt.strftime("%a, %d %b %Y %H:%M:%S +0000")
 
-
-def getSeriesList(file):
-    try:
-        titles = []
-        f = codecs.open(file, "rb", "utf-8")
-        for title in f.read().splitlines():
-            if len(title) == 0:
-                continue
-            title = title.replace(" ", ".")
-            titles.append(title)
-        f.close()
-        return titles
-    except UnicodeError:
-        print("Abbruch, es befinden sich ungueltige Zeichen in der Suchdatei!")
-    except IOError:
-        print("Abbruch, Suchdatei wurde nicht gefunden!")
-    except Exception, e:
-        print("Unbekannter Fehler: %s" %e) 
-
-
-def range_checkr(link, title):
+def range_checkr(link, title, language):
     pattern = re.match(".*S\d{2}E\d{2}-\w?\d{2}.*", title)
     if pattern is not None:
         range0 = re.sub(r".*S\d{2}E(\d{2}-\w?\d{2}).*",r"\1", title).replace("E","")
@@ -46,26 +21,20 @@ def range_checkr(link, title):
             NR = re.match("d\{2}", str(count))
             if NR is not None:
                 title1 = title_cut[0][0] + str(count) + ".*" + title_cut[0][-1]
-                range_parse(link, title1)
+                range_parse(link, title1, language)
             else:
                 title1 = title_cut[0][0] + "0" + str(count) + ".*" + title_cut[0][-1]
-                range_parse(link, title1)
+                range_parse(link, title1, language)
     else:
-        parse_download(link, title) 
+        parse_download(link, title, language) 
 
-def range_parse(series_url, search_title):
+def range_parse(series_url, search_title, language):
     req_page = requests.get(series_url).text
     soup = BeautifulSoup(req_page)
     titles = soup.findAll(text=re.compile(search_title))
-    for title in titles:
-       if quality !='480p' and quality in title:
-           print title
-           parse_download(series_url, title)
-       if quality =='480p' and not (('.720p.' in title) or ('.1080p.' in title)):
-           print title
-           parse_download(series_url, title)
+    parse_download(series_url, title, language)
 
-def parse_download(series_url, search_title):
+def parse_download(series_url, search_title, language):
     req_page = requests.get(series_url).text
     soup = BeautifulSoup(req_page)
     title = soup.find(text=re.compile(search_title))
@@ -77,23 +46,18 @@ def parse_download(series_url, search_title):
             if re.match(pattern, url):
                 print title
                 print url
-                make_rss(title,url)
+                make_rss(title, url, language)
 
-def make_rss(title, link):
-    outputFile.write("<item>\n")
-    outputFile.write("<title>"+title+"</title>\n")
-    outputFile.write("<link>"+link+"</link>\n")
-    outputFile.write("</item>\n")
+def make_rss(title, link, language):
+    outputFile.write("\t<item>\n")
+    outputFile.write("\t\t<title>"+language+" "+title+"</title>\n")
+    outputFile.write("\t\t<link>"+link+"</link>\n")
+    outputFile.write("\t</item>\n")
 
-
-
-
+    
 feed = feedparser.parse('http://serienjunkies.org/xml/feeds/episoden.xml')
-pattern = "|".join(getSeriesList(file)).lower()
-reject = rejectlist.replace(";","|").lower() if len(rejectlist) > 0 else "^unmatchable$"
 if hoster == "alle":
-	hoster = "."
-added_items = []
+    hoster = "."
 
 ## Schreibe RSS
 outputFile = open(outputFilename, "w")
@@ -103,61 +67,24 @@ outputFile.write("<rss version=\"2.0\">\n")
 outputFile.write("<channel>\n")
 outputFile.write("<title>SerienJunkies-RSS Generator</title>\n")
 outputFile.write("<description>Serienjunkies.org RSS Generator for Flexget</description>\n")
-outputFile.write("<link>theres no link</link>\n")
+outputFile.write("<link>Serienjunkies.org</link>\n")
 outputFile.write("<ttl> </ttl>\n")
 
 
+now = datetime.datetime.now()
 for post in feed.entries:
-	link = post.link
-	title = post.title
-	
-	if regex:
-		m = re.search(pattern,title.lower())
-		if not m and not "720p" in title and not "1080p" in title:
-			m = re.search(pattern.replace("480p","."),title.lower())
-			quality = "480p"
-		if m:
-			if "720p" in title.lower(): quality = "720p"
-			if "1080p" in title.lower(): quality = "1080p"
-			m = re.search(reject,title.lower())
-			if m and debug:
-				print("Abgelehnt: " + title)
-				continue
-			title = re.sub('\[.*\] ', '', post.title)
-			range_checkr(link,title)
-						
-	else:
-		if quality != '480p':
-			m = re.search(pattern,title.lower())
-			if m:
-				if language in title:
-					mm = re.search(quality,title.lower())
-					if mm:
-						mmm = re.search(reject,title.lower())
-						if mmm and debug:
-							print("Abgelehnt: " + title)
-							continue
-						title = re.sub('\[.*\] ', '', post.title)
-						range_checkr(link,title)
-
-		else:
-			m = re.search(pattern,title.lower())
-			if m:
-				if language in title:
-					if "720p" in title.lower() or "1080p" in title.lower():
-						continue
-					mm = re.search(reject,title.lower())
-					if mm and debug:
-						print("Abgelehnt: " + title)
-						continue
-					title = re.sub('\[.*\] ', '', post.title) 
+    feed_date = re.sub(r"(\w{3},\s\d{2}\s\w{3}\s\d{4}).*", r"\1", post.published)
+    today = re.sub(r"(\w{3},\s\d{2}\s\w{3}\s\d{4}).*", r"\1", formatDate(now))
+    if feed_date == today:
+        link = post.link
+        language = re.sub(r'\[(.*)\].*', r'\1', post.title)
+        title = re.sub('\[.*\] ', '', post.title)
+        #print title 
+        #print link
+        #print language
+        range_checkr(link,title,language)
 
 # Schreibe RSS footer
 outputFile.write("</channel>\n")
 outputFile.write("</rss>")
 outputFile.close()
-
-
-
-
-
